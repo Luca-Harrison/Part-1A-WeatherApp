a05ce579ddb80a18657f570b96cef8b52ebe90cd
Luca-Harrison <lh889@cam.ac.uk>
2025-05-14 15:44:48 +0100

    Add files via upload

--- state/weatherState.js
+++ state/weatherState.js
@@ -4,6 +4,23 @@ class WeatherState extends DefaultState {
       
       this.addRRect(5, 2, 90, 10, 4, colour(180, 200, 220));
       this.addRRect(5, 70, 90, 105, 4, colour(180, 200, 220));
+
+      let buttons = [];
+      
+      let inc = 56 / 7;
+      for (let i = 0; i < 7; i ++) {
+        buttons[i] = this.addButton(7, 13.5 + inc * i, 86, inc - 1, 4, colour(40), colour(15), colour(100), 2);
+      }
+      
+      let toggleButton = this.addButton(7, 94, 10, 5, 2, colour(200, 220, 240), colour(160, 180, 200));
+      
+      buttons[1].script = () => {
+        state.changeState(new TemperatureState(this.state, this.redMode));
+      };
+      
+      toggleButton.script = () => {
+        this.toggleRedMode();
+      }
       
       this.exc();
     }
