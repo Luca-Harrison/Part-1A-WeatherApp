258b81701d5504acfa17953248619bcc2c8c5ac2
Luca-Harrison <lh889@cam.ac.uk>
2025-05-14 15:23:46 +0100

    Add files via upload

--- drawing/shapes.js
+++ drawing/shapes.js
@@ -1,1 +1,47 @@
+function colour(r, g = r, b = r, a = 1) {
+  return {
+    r: r,
+    g: g,
+    b: b,
+    a: a
+  };
+}
 
+function fill(fillObj) {
+  ctx.fillStyle = fillObj;
+}
+
+function stroke(strokeObj) {
+  ctx.strokeStyle = strokeObj;
+}
+
+function strokeWidth(w) {
+  ctx.lineWidth = w;
+}
+
+function rect(x, y, w, h) {
+  ctx.beginPath();
+  ctx.fillRect(x, y, w, h);
+  ctx.fill();
+  ctx.stroke();
+}
+
+function circle(x, y, r) {
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, 2 * Math.PI);
+  ctx.fill();
+  ctx.stroke();
+}
+
+function rRect(x, y, w, h, r) {
+  ctx.beginPath();
+  
+  ctx.moveTo(x+r, y);
+  ctx.arcTo(x+w, y, x+w, y+r, r);
+  ctx.arcTo(x+w, y+h, x+w-r, y+h, r);
+  ctx.arcTo(x, y+h, x, y+h-r, r);
+  ctx.arcTo(x, y, x+r, y, r);
+
+  ctx.fill();
+  ctx.stroke();
+}
