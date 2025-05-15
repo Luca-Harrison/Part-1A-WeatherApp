9db269dd9d824e789c7c64c702d005822a135659
Luca-Harrison <lh889@cam.ac.uk>
2025-05-15 14:06:56 +0100

    Add files via upload

--- drawing/stateButton.js
+++ drawing/stateButton.js
@@ -7,14 +7,28 @@ class Button extends StateObject {
         this.w = w * width / 100;
         this.h = h * height / 100;
         this.r = r * width / 100;
+
+        this.scripts = [];
     }
-    script() {}
+    setW(w) {
+        this.w = w * width / 100;
+    }
+    setH(h) {
+        this.h = h * height / 100;
+    }
+    addScript(f) {
+        this.scripts.push(f);
+    }
     checkState() {
         this.click = mouse.click && 
                      mouse.x >= this.x && mouse.x <= this.x + this.w &&
                      mouse.y >= this.y && mouse.y <= this.y + this.h;
 
-        if (this.click) setTimeout(this.script, 60);
+        if (this.click) setTimeout(() => {
+            for (let i = 0; i < this.scripts.length; i++) 
+                this.scripts[i]();
+            }, 30);
+            
 
         this.state = mouse.x >= this.x && mouse.x <= this.x + this.w &&
                      mouse.y >= this.y && mouse.y <= this.y + this.h;
