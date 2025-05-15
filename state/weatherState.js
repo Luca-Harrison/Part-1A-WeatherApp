5b665dc2bed17380e7537b75c2a8ea85e76621ed
Oliver Bennett <omb31@cam.ac.uk>
2025-05-15 15:49:04 +0100

    Updated data to be in dictionary

--- state/weatherState.js
+++ state/weatherState.js
@@ -5,14 +5,14 @@ class WeatherState extends DefaultState {
       this.addRRect(5, 2, 90, 10, 4, colour(180, 200, 220));
       let bottomBar = this.addRRect(5, 90, 90, 9.5, 4, colour(180, 200, 220));
 
-      const names = ["Quality of Stargazing","Temperature","Cloud Cover","Rain","Visability","Fod Level","Light Level"];
-      const data = ["Excellent","15\u{00B0}C","10%","6% Chance","20Km","Low","Dark"];
+      const names = ["Quality of Stargazing","Temperature","Cloud Cover","Rain","Visability","Fog Level","Light Level"];
+      let data = { "Quality of Stargazing":"Excellent","Temperature":"15\u{00B0}C","Cloud Cover":"6%","Rain":"4% Chance","Visability":"20Km","Fog Level":"Low","Light Level":"Dark"}
       
       let inc = 76.5 / 7;
       for (let i = 0; i < 7; i ++) {
         this.addButton(7, 13.5 + inc * i, 86, inc - 1, 4, colour(40), colour(15), colour(100), 2);
         this.addText(names[i],12,16 + inc * i,4.5,colour(180, 200, 220))
-        this.addText(data[i],10,21 + inc * i,8,colour(180, 200, 220))
+        this.addText(data[names[i]],10,21 + inc * i,8,colour(180, 200, 220))
       }
 
       this.toggleButton = this.addButton(7, 94, 10, 5, 2, colour(200, 220, 240), colour(160, 180, 200));
