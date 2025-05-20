d08d37a4f6ad4199299cd6ff35c6e5be706f5a3d
SakthivelanPandy <sakthivelanpandy@gmail.com>
2025-05-20 14:53:33 +0100

    basic api and storage functionality

--- logic/api.js
+++ logic/api.js
@@ -4,12 +4,34 @@
 function getWeather() {
   lat = getLatitude();
   long = getLongitude();
+  console.log("Latitude: " + lat);
+  console.log("Longitude: " + long);
 
   units = getTempUnits();
+  console.log("Units: " + units);
+  if (units == "C") {
+    units = "celsius";
+  } else {
+    units = "fahrenheit";
+  }
 
-  const xhttpr = new XMLHttpRequest();
-  url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=cloud_cover,visibility&current=temperature_2m,precipitation,wind_speed_10m&forecast_days=1"
+  const url = "https://api.open-meteo.com/v1/forecast?latitude=" + lat + "&longitude=" + long + "&hourly=cloud_cover,visibility&current=temperature_2m,precipitation,wind_speed_10m&forecast_days=1&temperature_unit=" + units;
+  console.log("URL: " + url);
 
+  $.get(url, function (data) {
+    console.log(data);
+    const temperature = data.current.temperature_2m;
+    const precipitation = data.current.precipitation;
+    const wind_speed = data.current.wind_speed_10m;
+    const cloud_cover = data.hourly.cloud_cover[0];
+    const visibility = data.hourly.visibility[0];
+
+    console.log("Temperature: " + temperature);
+    console.log("Precipitation: " + precipitation);
+    console.log("Wind Speed: " + wind_speed);
+    console.log("Cloud Cover: " + cloud_cover);
+    console.log("Visibility: " + visibility);
+  })
 }
 
 
