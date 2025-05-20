5bbb49a56d234328287bbc3fc0fc59c6f5411d8f
Oliver Bennett <omb31@cam.ac.uk>
2025-05-20 14:34:15 +0100

    moved text

--- state/weatherState.js
+++ state/weatherState.js
@@ -3,7 +3,7 @@ class WeatherState extends DefaultState {
       super(state, redMode);
       
       this.addRRect(5, 2, 90, 10, 4, colour(180, 200, 220));
-      this.addText("Weather",26,9,12,colour(0,0,0))
+      this.addText("Weather",22,9.5,8,colour(0,0,0))
       let bottomBar = this.addRRect(5, 90, 90, 9.5, 4, colour(180, 200, 220));
 
       const names = ["Quality of Stargazing","Temperature","Cloud Cover","Rain","Visability","Fog Level","Light Level"];
@@ -12,11 +12,11 @@ class WeatherState extends DefaultState {
       let inc = 76.5 / 7;
       for (let i = 0; i < 7; i ++) {
         this.addButton(7, 13.5 + inc * i, 86, inc - 1, 4, colour(40), colour(15), colour(100), 2);
-        this.addText(names[i],12,16 + inc * i,4.5,colour(180, 200, 220))
-        this.addText(data[names[i]],10,21 + inc * i,8,colour(180, 200, 220))
+        this.addText(names[i],12,16 + inc * i,2,colour(180, 200, 220))
+        this.addText(data[names[i]],9,21.75 + inc * i,6,colour(180, 200, 220))
       }
 
-      this.toggleButton = this.addButton(7, 94, 10, 5, 2, colour(200, 220, 240), colour(160, 180, 200));
+      this.toggleButton = this.addButton(7, 90.75, 8, 8, 2, colour(200, 220, 240), colour(160, 180, 200));
 
       this.toggleButton.addScript( () => {
         this.toggleRedMode();
