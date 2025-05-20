d787295532fce92fb974e9629595efc379ac2ea1
SohamShah738 <sohamnshah123@gmail.com>
2025-05-20 15:34:51 +0100

    location page nearly done

--- state/locationState.js
+++ state/locationState.js
@@ -7,16 +7,20 @@ class LocationState extends DefaultState {
 
         // Search bar to search for locations
         this.addButton(5, 5, 90, 7, 5, colour(180, 200, 220), colour(160, 180, 200));
-        this.addText("Location", 18, 9.5, 3, colour(80, 80, 80)); 
+        this.addText("Location", 18, 9.5, 2.5, colour(80, 80, 80)); 
         this.addImage("images/search.svg", 85, 7, 6, 3); 
-        this.addImage("images/bars.svg", 8, 7, 6, 3)
+        this.addImage("images/bars.svg", 8, 7, 6, 3); 
 
         // Each individual location button 
-        let locations = ["Cambridge", "Girton", "London", "Manchester", "Plymouth", "Rochester", "Peterborough"]
+        let locations = [["Cambridge", "Cambridgeshire"], ["Girton", "Cambridgeshire"], 
+            ["London", "Greater London"], ["Manchester", "Greater Manchester"], ["Plymouth", "Devon"], 
+            ["Rochester", "Kent"], ["Peterborough", "Cambridgeshire"]]; 
+
         let inc = 10;
         for (let i = 0; i < 7; i ++) {
-            this.addButton(5, 20 + inc * i, 90, inc - 1, 4, colour(180, 200, 220));
-            this.addText(locations[i], 25, 24 + inc * i, 2, colour(0)); 
+            this.addButton(5, 20 + inc * i, 90, inc - 1, 4, colour(180, 200, 220), colour(160, 180, 200));
+            this.addText(locations[i][0], 25, 24 + inc * i, 2, colour(0)); 
+            this.addText(locations[i][1], 25, 27 + inc * i, 1.5, colour(80, 80, 80)); 
             this.addImage("images/location.svg", )
         }
 
