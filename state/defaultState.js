9baefc9ba01d643bd8ffcc245b9e5ec102c52a83
beaconmc <kimminkyumkim@gmail.com>
2025-05-20 15:49:37 +0100

    fixed images

--- state/defaultState.js
+++ state/defaultState.js
@@ -39,9 +39,9 @@ class DefaultState {
         let settingsButton = this.addButton(6, 94, 28, 5.5, 2, colour(180, 200, 220), colour(160, 180, 200));
         let weatherButton = this.addButton(36, 94, 28, 5.5, 2, colour(180, 200, 220), colour(160, 180, 200));
         let planetsButton = this.addButton(66, 94, 28, 5.5, 2, colour(180, 200, 220), colour(160, 180, 200));
-        this.addImage('SettingsImage.png', 16, 95, 8, 4);
-        this.addImage('CloudImage.png', 44, 94, 12, 6);
-        this.addImage('PlanetImage.svg', 75, 94.5, 10, 5);
+        this.addImage('images/SettingsImage.png', 16, 95, 8, 4);
+        this.addImage('images/CloudImage.png', 44, 94, 12, 6);
+        this.addImage('images/PlanetImage.svg', 75, 94.5, 10, 5);
 
         settingsButton.addScript(() => {
             this.state.changeState(new SettingState(this.state, this.redMode));
