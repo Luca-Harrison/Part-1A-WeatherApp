0e26a23cfecafeb210c0cbecd03fcba897a69a3d
Oliver Bennett <omb31@cam.ac.uk>
2025-05-20 19:40:15 +0100

    Removed console log message

--- main.js
+++ main.js
@@ -6,10 +6,9 @@ function update() {
   rect(0, 0, width, height);
 
   //Don't commit!!!
-  //state.changeState(new WeatherState());
+  state.changeState(new WeatherState());
 
   if (firstTime()) {
-    console.log("Yo")
     state.changeState(new LocationState(this.state, this.redMode));
   }
 
