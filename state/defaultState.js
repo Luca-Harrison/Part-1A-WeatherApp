4a93f50644e6f71267f8f9ba36277352e5a63ca4
Oliver Bennett <omb31@cam.ac.uk>
2025-05-22 15:39:47 +0100

    fixed problems

--- state/defaultState.js
+++ state/defaultState.js
@@ -1,85 +1,101 @@
 class DefaultState {
 
 
-  constructor(state, redMode) {
-    this.objectList = [];
-    
-    this.state = state;
-    this.redMode = redMode;
-  }
+    constructor(state, redMode) {
+        this.objectList = [];
 
-  addButton(x, y, w, h, r, fillColour, hoverColour, strokeColour, strokeWidth) {
-    let btn = new Button(x, y, w, h, r, fillColour, hoverColour, strokeColour, strokeWidth);
-    this.objectList.push(btn);
-    return btn;
-  }
+        this.state = state;
+        this.redMode = redMode;
+    }
 
-  addTextbox(defaultText, x, y, w, h, r, fillColour, typingColour, textColour, strokeColour, strokeWidth) {
-    let textbox = new StateTextbox(defaultText, x, y, w, h, r, fillColour, typingColour, textColour, strokeColour, strokeWidth);
-    this.objectList.push(textbox);
-    return textbox;
-  }
+    addButton(x, y, w, h, r, fillColour, hoverColour, strokeColour, strokeWidth) {
+        let btn = new Button(x, y, w, h, r, fillColour, hoverColour, strokeColour, strokeWidth);
+        this.objectList.push(btn);
+        return btn;
+    }
 
-  addSlider(x, y, w, h, r, fillColour, hoverColour, strokeColour, strokeWidth) {
-    let sld = new StateSlider(x, y, w, h, r, fillColour, hoverColour, strokeColour, strokeWidth);
-    this.objectList.push(sld);
-    return sld;
-  }
+    addTextbox(defaultText, x, y, w, h, r, fillColour, typingColour, textColour, strokeColour, strokeWidth) {
+        let textbox = new StateTextbox(defaultText, x, y, w, h, r, fillColour, typingColour, textColour, strokeColour, strokeWidth);
+        this.objectList.push(textbox);
+        return textbox;
+    }
 
-  addRRect(x, y, w, h, r, fillColour, strokeColour, strokeWidth) {
-    let rct = new RoundedRect(x, y, w, h, r, fillColour, strokeColour, strokeWidth);
-    this.objectList.push(rct);
-    return rct;
-  }
+    addSlider(x, y, w, h, r, fillColour, hoverColour, strokeColour, strokeWidth) {
+        let sld = new StateSlider(x, y, w, h, r, fillColour, hoverColour, strokeColour, strokeWidth);
+        this.objectList.push(sld);
+        return sld;
+    }
 
-  addText(txt, x, y, size, fillColour) {
-    let txtObj = new StateText(txt, x, y, size, fillColour);
-    this.objectList.push(txtObj);
-    return txtObj;
-  }
+    addRRect(x, y, w, h, r, fillColour, strokeColour, strokeWidth) {
+        let rct = new RoundedRect(x, y, w, h, r, fillColour, strokeColour, strokeWidth);
+        this.objectList.push(rct);
+        return rct;
+    }
 
-  addImage(src, x, y, w, h) {
-    let imgObj = new StateImage(src, x, y, w, h);
-    this.objectList.push(imgObj);
-    return imgObj;
-  }
+    addText(txt, x, y, size, fillColour) {
+        let txtObj = new StateText(txt, x, y, size, fillColour);
+        this.objectList.push(txtObj);
+        return txtObj;
+    }
 
-  addNavigationBar() {
+    addImage(src, x, y, w, h) {
+        let imgObj = new StateImage(src, x, y, w, h);
+        this.objectList.push(imgObj);
+        return imgObj;
+    }
 
-      let bottomBar = this.addRRect(5, 93, 90, 105, 4, colour(180, 200, 220));
+    addNavigationBar() {
 
-      let settingsButton = this.addButton(6, 94, 28, 5.5, 2, colour(180, 200, 220), colour(160, 180, 200));
-      let weatherButton = this.addButton(36, 94, 28, 5.5, 2, colour(180, 200, 220), colour(160, 180, 200));
-      let planetsButton = this.addButton(66, 94, 28, 5.5, 2, colour(180, 200, 220), colour(160, 180, 200));
-      this.addImage('images/SettingsImage.png', 16, 95, 8, 4);
-      this.addImage('images/CloudImage.png', 44, 94, 12, 6);
-      this.addImage('images/PlanetImage.svg', 75, 94.5, 10, 5);
+        let bottomBar = this.addRRect(5, 93, 90, 105, 4, colour(180, 200, 220));
 
-      settingsButton.addScript(() => {
-          this.state.changeState(new SettingState(this.state, this.redMode));
-      });
-      weatherButton.addScript(() => {
-          this.state.changeState(new WeatherState(this.state, this.redMode));
-      });
-      planetsButton.addScript(() => {
-          this.state.changeState(new PlanetState(this.state, this.redMode));
-      });
+        let settingsButton = this.addButton(6, 94, 28, 5.5, 2, colour(180, 200, 220), colour(160, 180, 200));
+        let weatherButton = this.addButton(36, 94, 28, 5.5, 2, colour(180, 200, 220), colour(160, 180, 200));
+        let planetsButton = this.addButton(66, 94, 28, 5.5, 2, colour(180, 200, 220), colour(160, 180, 200));
+        this.addImage('images/SettingsImage.png', 16, 95, 8, 4);
+        this.addImage('images/CloudImage.png', 44, 94, 12, 6);
+        this.addImage('images/PlanetImage.svg', 75, 94.5, 10, 5);
 
-  }
+        settingsButton.addScript(() => {
+            this.state.changeState(new SettingState(this.state, this.redMode));
+        });
+        weatherButton.addScript(() => {
+            this.state.changeState(new WeatherState(this.state, this.redMode));
+        });
+        planetsButton.addScript(() => {
+            this.state.changeState(new PlanetState(this.state, this.redMode));
+        });
 
-  exc() {
-    for (let i = 0; i < this.objectList.length; i ++)
-      this.objectList[i].setRedMode(this.redMode);
-  }
-  setRedMode(val) {
-    this.redMode = val;
-    this.exc();
-  }
-  update() {
-    this.script();
+    }
 
-    for (let i = 0; i < this.objectList.length; i++)
-      this.objectList[i].update();
-  }
-  script() { }
+    addBasicContent() {
+        this.addImage('graphs1.png', 8,40,84,27.6);
+
+        let back = this.addButton(8.5, 4.5, 10.2, 5.1, 4, colour(180,200,220),colour(167, 186, 204));
+        this.addImage('images/back-arrow.svg', 8.5,4.5,10,5);
+
+        let location = "Cambridge"
+
+        back.addScript(() => {
+            this.state.changeState(new WeatherState(this.state, this.redMode));
+        });
+
+        this.addText(location, 30, 20,4,colour(180, 200, 220));
+
+    }
+
+    exc() {
+        for (let i = 0; i < this.objectList.length; i ++)
+            this.objectList[i].setRedMode(this.redMode);
+    }
+    setRedMode(val) {
+        this.redMode = val;
+        this.exc();
+    }
+    update() {
+        this.script();
+
+        for (let i = 0; i < this.objectList.length; i++)
+            this.objectList[i].update();
+    }
+    script() { }
 }
