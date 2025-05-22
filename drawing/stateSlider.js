453495de5bfd5142c01f8ae0b5cd47243962c12f
Luca Harrison <lh889@cam.ac.uk>
2025-05-22 13:34:54 +0100

    'yes'

--- drawing/stateSlider.js
+++ drawing/stateSlider.js
@@ -1,6 +1,6 @@
 class StateSlider extends StateObject {    
-    constructor(x, y, w, h, r, fillColour = colour(0), hoverColour = colour(0), strokeColour = colour(0, 0, 0, 0), strokeWidth = 0) {
-        super(fillColour, hoverColour, strokeColour, strokeWidth);
+    constructor(x, y, w, h, r, fillColour = colour(0), hoverColour = colour(0), strokeColour = colour(0, 0, 0, 0)) {
+        super(fillColour, hoverColour, strokeColour, 0);
 
         this.x = x * width / 100;
         this.y = y * height / 100;
@@ -8,7 +8,14 @@ class StateSlider extends StateObject {
         this.h = h * height / 100;
         this.r = r * width / 100;
 
+        this.val = 1;
+
         this.scripts = [];
+
+        this.addScript(() => {
+            this.val = (mouse.x - this.x - this.h /2) / (this.w - this.h / 2);
+            this.val = this.val < 0 ? 0 : this.val;
+        });
     }
     setW(w) {
         this.w = w * width / 100;
@@ -34,7 +41,15 @@ class StateSlider extends StateObject {
                      mouse.y >= this.y && mouse.y <= this.y + this.h;
     }
     draw() {
+        stroke(`rgb(0, 0, 0, 0)`);
+        
+        fill(`rgb(${this.strokeColour.r}, ${this.redMode * this.strokeColour.g}, ${this.redMode * this.strokeColour.b}, ${this.strokeColour.a})`);
+
         rRect(this.x, this.y + 0.4 * this.h, this.w, 0.2 * this.h, 0.1 * this.h);
-        rRect(this.x, this.y, this.h, this.h, this.r);
+        
+        if (this.state == 0) fill(`rgb(${this.defaultColour.r}, ${this.redMode * this.defaultColour.g}, ${this.redMode * this.defaultColour.b}, ${this.defaultColour.a})`);
+        else fill(`rgb(${this.altColour.r}, ${this.redMode * this.altColour.g}, ${this.redMode * this.altColour.b}, ${this.altColour.a})`);
+
+        rRect(this.x + this.val * (this.w - this.h /2), this.y, this.h, this.h, this.r);
     }
 }
