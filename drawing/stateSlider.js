c83219c93c05c4a102669b037da069bebde18263
Luca Harrison <lh889@cam.ac.uk>
2025-05-22 15:37:02 +0100

    'Comments'

--- drawing/stateSlider.js
+++ drawing/stateSlider.js
@@ -8,7 +8,8 @@ class StateSlider extends StateObject { // Allow slider objects to be added to t
         this.h = h * height / 100;
         this.r = r * width / 100;
 
-        this.val = 1; // Value for the 
+        // Value for the slider
+        this.val = 1;
 
         this.scripts = [];
 
@@ -17,15 +18,21 @@ class StateSlider extends StateObject { // Allow slider objects to be added to t
             this.val = this.val < 0 ? 0 : this.val;
         });
     }
+
+    // Setters
     setW(w) {
         this.w = w * width / 100;
     }
     setH(h) {
         this.h = h * height / 100;
     }
+
+    // Allow scripts to be added to the objects
     addScript(f) {
         this.scripts.push(f);
     }
+
+    // Check if slider is being pressed and if so change state to on
     checkState() {
         let click = mouse.click && 
                      mouse.x >= this.x && mouse.x <= this.x + this.w &&
@@ -35,21 +42,30 @@ class StateSlider extends StateObject { // Allow slider objects to be added to t
             for (let i = 0; i < this.scripts.length; i++) 
                 this.scripts[i]();
             }, 30);
-            
+        // Time out required so that buttons that move dependent on other buttons can't 
+        // move into a button press and be triggered on the same frame. (This would 
+        // create unpredictable behavior).
 
         this.state = mouse.x >= this.x && mouse.x <= this.x + this.w &&
                      mouse.y >= this.y && mouse.y <= this.y + this.h;
     }
     draw() {
+        // Remove the stroke
         stroke(`rgb(0, 0, 0, 0)`);
         
+        // Set the slider colour with the correct red mode colour
         fill(`rgb(${this.strokeColour.r}, ${this.redMode * this.strokeColour.g}, ${this.redMode * this.strokeColour.b}, ${this.strokeColour.a})`);
 
+        // Draw the slider
         rRect(this.x, this.y + 0.4 * this.h, this.w, 0.2 * this.h, 0.1 * this.h);
         
+        // Set the colours of the object dependent on red mode value
+
         if (this.state == 0) fill(`rgb(${this.defaultColour.r}, ${this.redMode * this.defaultColour.g}, ${this.redMode * this.defaultColour.b}, ${this.defaultColour.a})`);
         else fill(`rgb(${this.altColour.r}, ${this.redMode * this.altColour.g}, ${this.redMode * this.altColour.b}, ${this.altColour.a})`);
 
+        // Draw the slider pointer
+        
         rRect(this.x + this.val * (this.w - this.h /2), this.y, this.h, this.h, this.r);
     }
 }
--- drawing/stateText.js
+++ drawing/stateText.js
@@ -1,5 +1,7 @@
-class StateText extends StateObject {
+class StateText extends StateObject { // Allow text to be added to the state
     constructor(txt, x, y, s, c) {
+
+        // Only required to set the fill colour of the text
         super(c, colour(0), colour(0), 0);
 
         this.txt = txt;
@@ -8,13 +10,19 @@ class StateText extends StateObject {
         this.y = y * height / 100;
         this.s = s * height / 100;
     }
+
+    // Setters
     setS(s) {
         this.s = s * height / 100;
     }
     setText(txt) {
         this.txt = txt;
     }
+
+
     draw() {
+
+        // Draw the text
         text(this.txt, this.x, this.y, this.s);
     }
 }
