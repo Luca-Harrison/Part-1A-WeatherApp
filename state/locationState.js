3b42b7e2c825bcaafe41f8e70b1e359c5f7ec1f0
SohamShah738 <sohamnshah123@gmail.com>
2025-05-22 14:42:33 +0100

    commented location state

--- state/locationState.js
+++ state/locationState.js
@@ -1,15 +1,14 @@
 class LocationState extends DefaultState {
     constructor(state, redMode) {
         super(state, redMode); 
-        // addButton(x, y, w, h, r, fillColour, hoverColour, strokeColour, strokeWidth)
-        // addText(txt, x, y, size, fillColour)
-
 
         // Search bar to search for locations
         this.addTextbox("Location", 5, 5, 90, 7, 5, colour(180, 200, 220), colour(160, 180, 200), colour(80));
+        // Add search and menu icons for the search bar
         this.addImage("images/search.svg", 85, 7, 6, 3); 
         this.addImage("images/bars.svg", 8, 7, 6, 3); 
 
+        // Red mode slider
         let redModeSlider = this.addSlider(10, 15, 50, 2, 2, colour(180, 200, 220), colour(160, 180, 200), colour(80));
         redModeSlider.addScript(() => {
             this.setRedMode(redModeSlider.val);
@@ -27,7 +26,9 @@ class LocationState extends DefaultState {
                 this.state.changeState(new WeatherState(this.state, this.redMode));
             })
             this.addText(locations[i][0], 23, 24 + inc * i, 2, colour(0)); 
-            this.addText(locations[i][1], 23, 27 + inc * i, 1.5, colour(80, 80, 80)); 
+            this.addText(locations[i][1], 23, 27 + inc * i, 1.5, colour(80, 80, 80));
+            
+            // Location icon
             this.addImage("images/location.svg", 10, 23 + inc * i, 8, 4); 
         }
 
