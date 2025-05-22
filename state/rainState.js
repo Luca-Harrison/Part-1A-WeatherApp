4abb2d8bc50bfeca2728d48220e6ff3741fea4bb
Oliver Bennett <omb31@cam.ac.uk>
2025-05-22 15:33:56 +0100

    Fixed branch

--- state/rainState.js
+++ state/rainState.js
@@ -1,23 +1,13 @@
 class RainState extends DefaultState {
 
-    constructor(state, redMode) {
+    constructor(state, redMode,value) {
         super(state, redMode);
 
         this.addRRect(5, 2, 90, 10, 4, colour(180, 200, 220));
         this.addText("Rain Chance",25,8.5,4.6,colour(0,0,0))
-        this.addImage('graphs1.png', 8,40,84,27.6);
 
-        let back = this.addButton(8.5, 4.5, 10.2, 5.1, 4, colour(180,200,220),colour(167, 186, 204));
-        this.addImage('images/back-arrow.svg', 8.5,4.5,10,5);
+        this.addBasicContent()
 
-        let location = "Cambridge"
-        let value = 12
-
-        back.addScript(() => {
-            this.state.changeState(new WeatherState(this.state, this.redMode));
-        });
-
-        this.addText(location, 30, 20,4,colour(180, 200, 220));
         this.addText("Rain Chance", 21, 25,5,colour(180, 200, 220));
         this.addText(value + "%", 35.5, 31,6,colour(180, 200, 220));
         //Adds bottom navigation bar
--- state/weatherState.js
+++ state/weatherState.js
@@ -1,70 +1,68 @@
-
-
 class WeatherState extends DefaultState {
 
-    constructor(state, redMode) {
-      super(state, redMode);
-      
-      this.addRRect(5, 2, 90, 10, 4, colour(180, 200, 220));
-      this.addText("Weather",31,9,4.6,colour(0,0,0))
+  constructor(state, redMode) {
+    super(state, redMode);
 
-      const names = ["Quality of Stargazing","Temperature","Cloud Cover","Rain","Visibility","Fog Level","Light Level"];
-      let data = { "Quality of Stargazing":"","Temperature":"","Cloud Cover":"","Rain":"","Visibility":"","Fog Level":"","Light Level":""}
-      let rawData = { "Quality of Stargazing":"Excellent","Temperature":"15","Cloud Cover":"6","Rain":"100","Visibility":"20","Fog Level":"Low","Light Level":"Dark"}
+    this.addRRect(5, 2, 90, 10, 4, colour(180, 200, 220));
+    this.addText("Weather",31,9,4.6,colour(0,0,0))
 
-      //Formats data so that it can be displayed in a readable way
-      function formatData(){
-        data["Quality of Stargazing"] = rawData["Quality of Stargazing"];
-        data["Temperature"] = rawData["Temperature"] + "\u{00B0}"
-        data["Cloud Cover"] = rawData["Cloud Cover"] + "%"
-        data["Rain"] = rawData["Rain"] + "% Chance";
-        data["Visibility"] = rawData["Visibility"] + "Km";
-        data["Fog Level"] = rawData["Fog Level"];
-        data["Light Level"] = rawData["Light Level"];
-      }
+    const names = ["Quality of Stargazing","Temperature","Cloud Cover","Rain","Visibility","Fog Level","Light Level"];
+    let data = { "Quality of Stargazing":"","Temperature":"","Cloud Cover":"","Rain":"","Visibility":"","Fog Level":"","Light Level":""}
+    let rawData = { "Quality of Stargazing":"Excellent","Temperature":"15","Cloud Cover":"6","Rain":"12","Visibility":"20","Fog Level":"Low","Light Level":"Dark"}
 
-      formatData();
+    //Formats data so that it can be displayed in a readable way
+    function formatData(){
+      data["Quality of Stargazing"] = rawData["Quality of Stargazing"];
+      data["Temperature"] = rawData["Temperature"] + "\u{00B0}"
+      data["Cloud Cover"] = rawData["Cloud Cover"] + "%"
+      data["Rain"] = rawData["Rain"] + "% Chance";
+      data["Visibility"] = rawData["Visibility"] + "Km";
+      data["Fog Level"] = rawData["Fog Level"];
+      data["Light Level"] = rawData["Light Level"];
+    }
 
-      let buttons = [];
+    formatData();
 
-      let inc = 79 / 7;
-      for (let i = 0; i < 7; i ++) {
-        buttons[i] = this.addButton(7, 13.5 + inc * i, 86, inc - 1, 4, colour(40), colour(15), colour(100), 2);
-        this.addText(names[i],9,17.2 + inc * i,2.5,colour(180, 200, 220))
-        this.addText(data[names[i]],9,22.4 + inc * i,4,colour(180, 200, 220))
-      }
+    let buttons = [];
 
-      buttons[1].addScript(() => {
-        this.state.changeState(new TemperatrueState(this.state, this.redMode));
-      });
+    let inc = 79 / 7;
+    for (let i = 0; i < 7; i ++) {
+      buttons[i] = this.addButton(7, 13.5 + inc * i, 86, inc - 1, 4, colour(40), colour(15), colour(100), 2);
+      this.addText(names[i],9,17.2 + inc * i,2.5,colour(180, 200, 220))
+      this.addText(data[names[i]],9,22.4 + inc * i,4,colour(180, 200, 220))
+    }
 
-      buttons[2].addScript(() => {
-        this.state.changeState(new CloudCoverState(this.state, this.redMode));
-      });
+    buttons[1].addScript(() => {
+      this.state.changeState(new TemperatrueState(this.state, this.redMode, rawData["Temperature"]));
+    });
 
-      buttons[3].addScript(() => {
-        this.state.changeState(new RainState(this.state, this.redMode));
-      });
+    buttons[2].addScript(() => {
+      this.state.changeState(new CloudCoverState(this.state, this.redMode, rawData["Cloud Cover"]));
+    });
 
-      //Function to update the data
-      /**
-       * @param newData - Object containing values as text
-       * Valid attribute names are: Quality of Stargazing, Temperature, Cloud Cover, Rain
-       * Visibility, Fog Level, Light Level
-       */
-      function updateData(newData) {
-        for (let key in newData){
-          if (key in data){
-            rawData[key] = newData[key]
-          } else {
-            console.log(key + " is not a valid key in data dictionary")
-          }
+    buttons[3].addScript(() => {
+      this.state.changeState(new RainState(this.state, this.redMode, rawData["Rain"]));
+    });
+
+    //Function to update the data
+    /**
+     * @param newData - Object containing values as text
+     * Valid attribute names are: Quality of Stargazing, Temperature, Cloud Cover, Rain
+     * Visibility, Fog Level, Light Level
+     */
+    function updateData(newData) {
+      for (let key in newData){
+        if (key in data){
+          rawData[key] = newData[key]
+        } else {
+          console.log(key + " is not a valid key in data dictionary")
         }
-        formatData();
       }
+      formatData();
+    }
 
-      //Adds bottom navigation bar
-      this.addNavigationBar();
-      this.exc();
+    //Adds bottom navigation bar
+    this.addNavigationBar();
+    this.exc();
   }
 }
